be0eb9839ba24b0b7d1a04ecabc3764a5ba37741
Jason Field <Jason@avon-lea.co.uk>
2021-10-22 18:55:53 +0100

    Update rules.rb

--- resources/rules.rb
+++ resources/rules.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook snort
+# Resource rules
+#
+# Copyright 2010-2017, Chef Software, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 property :oinkcode, [String, nil] # Supply this to download registered/subscriber rules
 property :tar_name, String, default: 'community-rules.tar.gz' # Override this if you have an Oink code and know the tar you want to download
 property :conf_dir, String, default: '/etc/snort'
