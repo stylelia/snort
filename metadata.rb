198ea1413e706d7bab3de2721b6d4dbae74905b3
Jason Field <Jason@avon-lea.co.uk>
2021-10-22 18:51:41 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,11 @@ name             "snort"
 maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
-description      "Installs Snort IDS packages"
+description      'Installs Snort IDS packages'
 version          '5.0.2'
 chef_version     '>= 15.0'
-source_url       "https://github.com/sous-chefs/snort"
-issues_url       "https://github.com/sous-chefs/snort/issues"
+source_url       'https://github.com/sous-chefs/snort'
+issues_url       'https://github.com/sous-chefs/snort/issues'
 
 %w(ubuntu debian redhat centos fedora scientific amazon oracle).each do |os|
   supports os
